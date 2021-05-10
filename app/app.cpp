3c1416f45118a2611229221c22b08c06db64110b
Joshua Inovero <78135477+joshuainovero@users.noreply.github.com>
2021-05-10 11:54:22 +0800

    Add project files.

---
+++ app/app.cpp
@@ -0,0 +1,97 @@
+#define CURL_STATICLIB
+#include <Windows.h>
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <curl/curl.h>
+#include <json/json.h>
+
+namespace
+{
+    std::size_t callback(
+        const char* in,
+        std::size_t size,
+        std::size_t num,
+        std::string* out)
+    {
+        const std::size_t totalBytes(size * num);
+        out->append(in, totalBytes);
+        return totalBytes;
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow)
+{
+    while (true) {
+        const std::string url(""); //JSON file from URL
+
+        CURL* curl = curl_easy_init();
+
+        // Set remote URL.
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        // Don't bother trying IPv6, which would increase DNS resolution time.
+        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+        // Don't wait forever, time out after 10 seconds.
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+
+        // Follow HTTP redirects if necessary.
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        // Response information.
+        long httpCode(0);
+        std::unique_ptr<std::string> httpData(new std::string());
+
+        // Hook up data handling function.
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+
+        // Hook up data container (will be passed as the last parameter to the
+        // callback handling function).  Can be any pointer type, since it will
+        // internally be passed as a void pointer.
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+        // Run our HTTP GET command, capture the HTTP response code, and clean up.
+        curl_easy_perform(curl);
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+        curl_easy_cleanup(curl);
+
+        if (httpCode == 200)
+        {
+            std::cout << "\nGot successful response from " << url << std::endl;
+
+            // Response looks good - done using Curl now.  Try to parse the results
+            // and print them out.
+            Json::Value jsonData;
+            Json::Reader jsonReader;
+
+            if (jsonReader.parse(*httpData.get(), jsonData))
+            {
+                std::cout << "Successfully parsed JSON data" << std::endl;
+                std::cout << "\nJSON data received:" << std::endl;
+                std::cout << jsonData.toStyledString() << std::endl;
+
+                std::cout << jsonData["state"] << std::endl;
+                if (jsonData["state"].asString() == "True")
+                    system("shutdown /s /t 5"); //Shutdown
+
+            }
+            else
+            {
+                std::cout << "Could not parse HTTP data as JSON" << std::endl;
+                std::cout << "HTTP data was:\n" << *httpData.get() << std::endl;
+                return 1;
+            }
+        }
+        else
+        {
+            std::cout << "Couldn't GET from " << url << " - exiting" << std::endl;
+            return 1;
+        }
+        Sleep(5000);
+    }
+
+    return 0;
+}
